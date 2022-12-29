eeac8be15fb1f9306dccc4b6551fb9a177a331ff
sd6040 <sd60@illinois.edu>
2022-12-29 17:31:31 -0600

    collatz sequence function

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 import os
+import igraph
+from igraph import Graph
 
 print("hi")
-print("test")
+print("bye")
+
+def collatz(num):
+    list = []
+    ## add current number to list and set num to next number in seq
+    while num > 1:
+        list.append(num)
+        num = collatz_helper(num)
+    ## loop breaks when num = 1 --> add 1 to list here
+    list.append(1)
+    print(*list, sep = ", ")
+
+## returns next number in sequence
+def collatz_helper(num):
+    if (num % 2 == 0):
+        return int(num / 2)
+    else:
+        return 3 * num + 1
+
+if __name__ == '__main__':
+    collatz(13)
