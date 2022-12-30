3eae7e37e404f55fa92caf98fb5bd113196e94f9
Tyler Cushing <tylercushing@Tylers-MacBook-Pro-4.local>
2022-12-29 19:42:32 -0600

    Implemented a rough start

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import os
 import igraph
 from igraph import Graph
 
-print("hi")
-print("lol")
-
 def collatz(num):
     list = []
     ## add current number to list and set num to next number in seq
@@ -13,7 +10,7 @@ def collatz(num):
         num = collatz_helper(num)
     ## loop breaks when num = 1 --> add 1 to list here
     list.append(1)
-    print(*list, sep = ", ")
+    return list
 
 ## returns next number in sequence
 def collatz_helper(num):
@@ -23,4 +20,8 @@ def collatz_helper(num):
         return 3 * num + 1
 
 if __name__ == '__main__':
+    num_to_start_from = 13
+    vertex_set = collatz(13)
+    vertex_set_as_str = [str(x) for x in vertex_set]
+    print(vertex_set_as_str)
     collatz(13)
--- tree plotter.py
+++ tree plotter.py
@@ -1,39 +1,36 @@
 import igraph as ig
 import matplotlib.pyplot as plt
 
-# Construct a graph with 3 vertices
-n_vertices = 3
-edges = [(0, 1), (0, 2), (0, 3), (0, 4), (1, 2), (1, 3), (1, 4), (3, 4)]
-g = ig.Graph(n_vertices, edges)
+# #functions to return the nodes in the tree
+def collatz(num):
+    list = []
+    ## add current number to list and set num to next number in seq
+    while num > 1:
+        list.append(num)
+        num = collatz_helper(num)
+    ## loop breaks when num = 1 --> add 1 to list here
+    list.append(1)
+    return list
 
-# Set attributes for the graph, nodes, and edges
-g["title"] = "Small Social Network"
-g.vs["name"] = ["Daniel Morillas", "Kathy Archer", "Kyle Ding", "Joshua Walton", "Jana Hoyer"]
-g.vs["gender"] = ["M", "F", "F", "M", "F"]
-g.es["married"] = [False, False, False, False, False, False, False, True]
+## returns next number in sequence
+def collatz_helper(num):
+    if (num % 2 == 0):
+        return int(num / 2)
+    else:
+        return 3 * num + 1
 
-# Set individual attributes
-g.vs[1]["name"] = "Kathy Morillas"
-g.es[0]["married"] = True
+num_to_start_from = 129
+vertex_set = collatz(num_to_start_from)
+vertex_set_as_str = [str(x) for x in vertex_set]
 
-# Plot in matplotlib
-# Note that attributes can be set globally (e.g. vertex_size), or set individually using arrays (e.g. vertex_color)
+
+print(vertex_set)
+index_of_start = vertex_set.index(num_to_start_from)
+g = ig.Graph(n = len(vertex_set), directed=True)
 fig, ax = plt.subplots(figsize=(5,5))
-ig.plot(
-    g,
-    target=ax,
-    layout="circle", # print nodes in a circular layout
-    vertex_size=0.1,
-    vertex_color=["steelblue" if gender == "M" else "salmon" for gender in g.vs["gender"]],
-    vertex_frame_width=4.0,
-    vertex_frame_color="white",
-    vertex_label=g.vs["name"],
-    vertex_label_size=7.0,
-    edge_width=[2 if married else 1 for married in g.es["married"]],
-    edge_color=["#7142cf" if married else "#AAA" for married in g.es["married"]]
-)
+g.add_edges([(1,0),(2,1), (0,3)])
+g.vs["label"] = vertex_set_as_str
+layout = g.layout_reingold_tilford(mode="in", root=[index_of_start])
 
+ig.plot(g, target=ax, layout=layout, vertex_size=0.6)
 plt.show()
-
-# Save the graph as an image file
-fig.savefig('social_network.png')
