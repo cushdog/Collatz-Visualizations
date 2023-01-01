4f1de1081c1543429a72fd5db306a91990206a32
sd6040 <sd60@illinois.edu>
2023-01-01 13:21:48 -0600

    added edmund harris visualization

--- turtle_visualizations.py
+++ turtle_visualizations.py
@@ -9,7 +9,7 @@ def collatz(num):
     ## loop breaks when num = 1 --> add 1 to list here
     num_list.append(1)
     num_list.reverse()
-    print(*num_list, sep = ", ")
+    ## print(*num_list, sep = ", ")
     return num_list
 
 ## returns next number in sequence
@@ -17,13 +17,14 @@ def collatz_helper(num):
     if (num % 2 == 0):
         return int(num / 2)
     else:
-        return 3 * num + 1
+        ## the edmund harris one uses / 2 for some reason???
+        return int((3 * num + 1) / 2)
 
-def collatz_visualizer_animated(end):
+def collatz_visualization_animated(end):
     ## change angle & length to modify visualization
     length = 5
     angle = 5
-    origin = (0, 0)
+    my_origin = (0, 0)
     ## turtle settings
     turtle.degrees()
     turtle.pencolor("red")
@@ -35,7 +36,7 @@ def collatz_visualizer_animated(end):
         turtle.tracer(False) #skips tracer animation to speed up code
         ## set position of turtle back to origin (does not reset direction of turtle)
         turtle.penup()
-        turtle.setposition(origin)
+        turtle.setposition(my_origin)
         turtle.pendown()
         for i in collatz_seq:
             if (i % 2 == 0):
@@ -46,14 +47,14 @@ def collatz_visualizer_animated(end):
     ## prevents window from automatically closing 
     turtle.done()
 
-def collatz_visualizer(end):
+def collatz_visualization(end):
     ## change angle & length to modify visualization
     length = 5
     angle = 5
-    origin = (0, 0)
+    my_origin = (0, 0)
     ## turtle settings
     turtle.degrees()
-    turtle.pencolor("red")
+    turtle.pencolor("#045a87")
     turtle.pensize(1.1)
     turtle.bgcolor(0, 0, 0)
     turtle.tracer(False) #skips tracer animation to speed up code
@@ -62,7 +63,7 @@ def collatz_visualizer(end):
         collatz_seq = collatz(num)
         ## set position of turtle back to origin (does not reset direction of turtle)
         turtle.penup()
-        turtle.setposition(origin)
+        turtle.setposition(my_origin)
         turtle.pendown()
         for i in collatz_seq:
             if (i % 2 == 0):
@@ -73,6 +74,35 @@ def collatz_visualizer(end):
     ## prevents window from automatically closing 
     turtle.done()
 
+def collatz_harris_visualization(end):
+    ## change angle & length to modify visualization
+    length = 10
+    angle = 0.08
+    my_origin = (-400, -400)
+    ## turtle settings
+    turtle.radians()
+    turtle.pencolor("#045a87")
+    turtle.bgcolor(0, 0, 0)
+    turtle.tracer(False) #skips tracer animation to speed up code
+    # turtle.pensize(1.1)
+    ## draws a line depending on collatz seq for each number
+    for num in range (end):
+        collatz_seq = collatz(num)
+        ## set position of turtle back to origin (does not reset direction of turtle)
+        turtle.penup()
+        turtle.setposition(my_origin)
+        turtle.setheading(3.141)
+        turtle.pendown()
+        for i in range (len(collatz_seq) - 1):
+            if (collatz_seq[i + 1] == 2 * collatz_seq[i]):
+                turtle.right(2 * angle)
+            else:
+                turtle.left(angle) 
+            turtle.forward(length)
+    ## prevents window from automatically closing 
+    turtle.done()
+
 if __name__ == '__main__':
-    ## collatz_visualizer(1000) ## faster but not animated
-    collatz_visualizer_animated(500) ## animated but takes much longer
+    ## collatz_visualization(4000) ## faster but not animated
+    ## collatz_visualization_animated(1000) ## animated but takes much longer
+    collatz_harris_visualization(4000) ## edmund harris visualization
